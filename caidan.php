a62a37c3acb365bfd3033b64bc7958825a705f09
亠口冗 <91411081+ll2310782478@users.noreply.github.com>
2022-09-26 09:43:40 +0800

    Add files via upload

---
+++ caidan.php
@@ -0,0 +1,13 @@
+<?php
+$cai=array("name"=>"肉夹馍","price"=>10,"picture"=>"img/https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fbkimg.cdn.bcebos.com%2Fpic%2Fd833c895d143ad4bd113a4d37e4a4dafa40f4bfba8c5&refer=http%3A%2F%2Fbkimg.cdn.bcebos.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1666748331&t=42852a68c3d2ba59452a4e607648a6f0");
+
+
+foreach($cai as $key=>$i){
+    echo $key;
+    echo ":";
+    echo $i;
+    echo "<br>";
+
+
+}
+?>
